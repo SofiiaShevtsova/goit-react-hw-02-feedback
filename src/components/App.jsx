67fdd9bc6536029e6bc5108c750d830765216421
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-13 19:29:48 +0200

    clean project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,3 @@
-import { ProfileCard } from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
-
 export const App = () => {
   return (
     <div
@@ -16,16 +7,6 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <ProfileCard
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions} />.
     </div>
   );
 };
